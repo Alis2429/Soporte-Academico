4ad55bd74665e7de7dc3495bea487a33816049fb
Alisson de la Cruz <delacruzsanchezalissonjadhira@gmail.com>
2026-09-21 19:15:03 -0500

    Req. 1: implementa registro de solicitudes

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print("Sistema de Soporte Académico")
+
+
+def registrar_solicitud(codigo, nombre, tipo_consulta, descripcion):
+    solicitud = {
+        "codigo": codigo,
+        "nombre": nombre,
+        "tipo_consulta": tipo_consulta,
+        "descripcion": descripcion
+    }
+    return solicitud
+
+
+solicitud = registrar_solicitud(
+    "N00543943",
+    "Alisson",
+    "matrícula",
+    "Consulta sobre inscripción de cursos"
+)
+
+print("\n--- Solicitud registrada ---")
+print("Código:", solicitud["codigo"])
+print("Nombre:", solicitud["nombre"])
+print("Tipo:", solicitud["tipo_consulta"])
+print("Descripción:", solicitud["descripcion"])
