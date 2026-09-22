4ad563ceab098b015b369621f9013b8101196fab
Alisson de la Cruz <delacruzsanchezalissonjadhira@gmail.com>
2026-09-21 19:36:17 -0500

    Req. 4: implementa menu principal

--- main.py
+++ main.py
@@ -1,32 +1,11 @@
 print("Sistema de Soporte Académico")
 
 
-def registrar_solicitud(codigo, nombre, tipo_consulta, descripcion):
-    solicitud = {
-        "codigo": codigo,
-        "nombre": nombre,
-        "tipo_consulta": tipo_consulta,
-        "descripcion": descripcion
-    }
-    return solicitud
+def mostrar_menu():
+    print("\n--- MENÚ PRINCIPAL ---")
+    print("1. Registrar solicitud")
+    print("2. Consultar solicitud")
+    print("3. Salir")
 
 
-def validar_codigo(codigo):
-    return codigo.strip() != "" and len(codigo) >= 5
-
-
-def validar_tipo_consulta(tipo):
-    tipos_validos = ["matrícula", "pagos", "constancia", "plataforma", "otro"]
-    return tipo.lower() in tipos_validos
-
-
-solicitud = registrar_solicitud(
-    "N00543943",
-    "Alisson",
-    "matrícula",
-    "Consulta sobre inscripción de cursos"
-)
-
-print("\n--- Validaciones ---")
-print("Código válido:", validar_codigo(solicitud["codigo"]))
-print("Tipo válido:", validar_tipo_consulta(solicitud["tipo_consulta"]))
+mostrar_menu()tipo_consulta"]))
